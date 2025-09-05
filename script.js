022df921d9c9d811a6819f172068603f25e86f2f
dhilruckshan <dr.dhilruckshan.periyasamy@gmail.com>
2025-09-05 20:15:31 +0530

    add command

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ var data = document.getElementById("data");
 
 function addtask()
 {
-    var task = document.createElement("div");
+    var task = document.createElement("div");// create div element
     task.className = "task";
-    var taskval = document.createElement("div");
+
+    var taskval = document.createElement("div");// create div element
     taskval.className ="taskval";
-    var task_text =document.createElement("p");
+
+    var task_text =document.createElement("p");// create para element
     task_text.textContent = data.value;
+
+
     taskval.append(task_text);
     task.append(taskval);
     var butt = document.createElement("div");
